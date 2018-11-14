9766c6598afa1caf2d160c0e69a73cff4657951a
HunterKnott <hunterknott00@gmail.com>
2018-11-14 12:19:56 -0700

    Added getters and setters for data members

--- src/island/model/Island.java
+++ src/island/model/Island.java
@@ -7,4 +7,41 @@ public class Island
 	private String Name;
 	private int Inhabitants;
 	private boolean isDesertIsland;
+	//_________________________________
+	public Island()
+	{
+		this.Name = "";
+		this.Inhabitants = 0;
+		this.isDesertIsland = false;
+	}
+	//_________________________________
+	public String getName()
+	{
+		return Name;
+	}
+	//---------------------------------
+	public int getInhabitants()
+	{
+		return Inhabitants;
+	}
+	//---------------------------------
+	public boolean getIsDesertIsland()
+	{
+		return isDesertIsland;
+	}
+	//__________________________________
+	public void setName(String Name)
+	{
+		this.Name = Name;
+	}
+	//----------------------------------
+	public void setInhabitants(int Inhabitants)
+	{
+		this.Inhabitants = Inhabitants;
+	}
+	//----------------------------------
+	public void setIsDesertIsland(boolean isDesertIsland)
+	{
+		this.isDesertIsland = isDesertIsland;
+	}
 }
